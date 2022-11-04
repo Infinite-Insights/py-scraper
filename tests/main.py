0c33b5ccd79ddd6e8f81e3e525afa0c5f42d5a86
Allie .S Ubisse <60136259+AllieUbisse@users.noreply.github.com>
2022-11-04 17:17:04 +0000

    update fast api test script

---
+++ tests/main.py
@@ -0,0 +1,36 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+    
+def test_fast_api():
+    assert 1==1, f"test "
+    print("testing test_fast_api")
+
+
+
+# test_fast_api()
